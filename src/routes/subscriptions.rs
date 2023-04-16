47045efe8f1aa1a9a052260ed5d177071b759f6d
Youngwook-Jeon <ela87@naver.com>
2023-04-16 18:03:01 +0900

    Use tracing crate for logging

--- src/routes/subscriptions.rs
+++ src/routes/subscriptions.rs
@@ -10,7 +10,11 @@ pub struct FormData {
 }
 
 pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> HttpResponse {
-    log::info!("Saving new subscriber details in the db");
+    let request_id = Uuid::new_v4();
+    tracing::info!(
+        "request_id {} - Saving new subscriber details in the db",
+        request_id
+    );
     match sqlx::query!(
         r#"
     INSERT INTO subscriptions (id, email, name, subscribed_at)
@@ -25,11 +29,18 @@ pub async fn subscribe(form: web::Form<FormData>, pool: web::Data<PgPool>) -> Ht
     .await
     {
         Ok(_) => {
-            log::info!("New subscriber details have been saved");
+            tracing::info!(
+                "request_id {} - New subscriber details have been saved",
+                request_id
+            );
             HttpResponse::Ok().finish()
         }
         Err(e) => {
-            log::error!("Failed to execute query: {:?}", e);
+            tracing::error!(
+                "request_id {} - Failed to execute query: {:?}",
+                request_id,
+                e
+            );
             HttpResponse::InternalServerError().finish()
         }
     }
